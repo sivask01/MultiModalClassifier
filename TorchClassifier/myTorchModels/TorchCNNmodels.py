cb587cfdd315262d0baa8c843813390cf0d42887
Sivakrishna Yaganti <44549091+sivask01@users.noreply.github.com>
2024-05-03 22:30:38 -0700

    Update TorchCNNmodels.py

--- TorchClassifier/myTorchModels/TorchCNNmodels.py
+++ TorchClassifier/myTorchModels/TorchCNNmodels.py
@@ -384,37 +384,37 @@ def create_lenet(numclasses, img_shape):
 
 class AlexNet(nn.Module):
     def __init__(self, output_dim):
-        super().__init__()
+        super(AlexNet, self).__init__()
         
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 2, 1), #in_channels, out_channels, kernel_size, stride, padding
-            nn.MaxPool2d(2), #kernel_size
-            nn.ReLU(inplace = True),
-            nn.Conv2d(64, 192, 3, padding = 1),
-            nn.MaxPool2d(2),
-            nn.ReLU(inplace = True),
-            nn.Conv2d(192, 384, 3, padding = 1),
-            nn.ReLU(inplace = True),
-            nn.Conv2d(384, 256, 3, padding = 1),
-            nn.ReLU(inplace = True),
-            nn.Conv2d(256, 256, 3, padding = 1),
-            nn.MaxPool2d(2),
-            nn.ReLU(inplace = True)
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),  # in_channels, out_channels, kernel_size, stride, padding
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
         )
         
         self.classifier = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(256 * 2 * 2, 4096),
-            nn.ReLU(inplace = True),
+            nn.ReLU(inplace=True),
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
-            nn.ReLU(inplace = True),
+            nn.ReLU(inplace=True),
             nn.Linear(4096, output_dim),
         )
 
     def forward(self, x):
         x = self.features(x)
-        h = x.view(x.shape[0], -1)
+        h = x.view(x.size(0), -1)
         x = self.classifier(h)
         return x, h
 
